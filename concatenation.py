21953bc9d2650fd2d975a37aee0a87cdb790e31e
Ali-Ahmad-1 <meetaliahmadnow@gmail.com>
2023-07-24 08:21:33 -0400

    combine two python string

---
+++ concatenation.py
@@ -0,0 +1,7 @@
+#combine two strings
+#I'm writing our full name with the help of multiples string.
+
+first_name = 'Ali-'
+last_name = 'Ahmad-1'
+full_name = first_name + ' ' + last_name
+print(full_name)

27fec8c22cb8e7bc2de98db6a5830f092c71f3d1
Ali-Ahmad-1 <meetaliahmadnow@gmail.com>
2023-07-24 08:05:25 -0400

    name + string

--- strings.py
+++ strings.py
@@ -1,2 +1,7 @@
 # print string on a screen:
+# Varible and string: == varible is used to store value: == string is a set of character asign in single quote or double quote:
 print("I'm String");
+
+# My name print with a string
+name = "Ali-Ahmad-1"
+print(name)

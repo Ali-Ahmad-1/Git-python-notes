70d265660ea56509b21e5991263e92dbfdf414e7
Ali-Ahmad-1 <meetaliahmadnow@gmail.com>
2023-07-25 03:40:35 -0400

    combine output from the list

---
+++ lists.py
@@ -0,0 +1,23 @@
+# Store s series of item in a particular order, i access item using an index, or within a loop:=
+
+#Making a list:
+Cars = ['BMW' , 'AUDI' , 'HONDA']
+#output first car in a list
+first_car = Cars[0]
+print(first_car)
+
+# Output last car in a list
+last_car = Cars[-1]
+print(last_car)
+
+# both output in a single line
+car = ['bmw','audi','honda']
+firstcar1 = car[0]
+lastcar2 = car[-1]
+print(firstcar1 + ' ' + lastcar2)
+
+
+
+# loop through a list
+for car in car:
+    car.append
